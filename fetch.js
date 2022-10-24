8b5c6e64af08c316171187745948670c087d5ad2
Gustavo Brandão <gugabgta3@gmail.com>
2022-10-24 17:57:58 -0300

    Operation is now async Reads all directories in default path

--- fetch.js
+++ fetch.js
@@ -1,11 +1,26 @@
 #!/bin/node
 import shell from 'shelljs'
 import chalk from 'chalk'
+import fs from 'fs'
 
-let dirs = ['core', 'tim', 'gsim-core-go', 'gsim-tim-go']
-dirs.forEach( dir => {
-    shell.cd('/home/gustavo/Projects/' + dir)
-    shell.echo(chalk.blue('analisando... ') + chalk.red(dir))
-    shell.exec('git fetch')
+const files = fs.readdirSync('/home/gustavo/Projects/', { withFileTypes: true })
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name)
+
+async function fetch(dir) {
+    shell.cd('/home/gustavo/Projects/' + dir, {'async': false})
+    await shell.exec('git fetch', {'async': true})
+    shell.echo(chalk.green('fetched /home/gustavo/Projects/' + dir))
+}
+
+let searches = new Promise((resolve, reject) => {
+    files.forEach(async (dir) => {
+        shell.echo(chalk.blue('analisando... ') + chalk.red(dir))
+        await fetch(dir)
+        resolve()
+    })
 })
-shell.echo(chalk.green('Feito'))
+
+searches.then(() => {
+    shell.echo(chalk.green('Feito'))
+})
