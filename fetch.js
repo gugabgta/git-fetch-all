7371320355d7f746a47b52803d8e3834e9283779
Gustavo Brandão <gugabgta3@gmail.com>
2023-06-12 08:33:48 -0300

    Project directory as a const
    Translated to english

--- fetch.js
+++ fetch.js
@@ -3,24 +3,26 @@ import shell from 'shelljs'
 import chalk from 'chalk'
 import fs from 'fs'
 
-const files = fs.readdirSync('/home/gustavo/Projects/', { withFileTypes: true })
+const projects_dir = '/home/gustavo/Projects/'
+
+const files = fs.readdirSync(projects_dir, { withFileTypes: true })
     .filter(dir => dir.isDirectory())
     .map(dir => dir.name)
 
 async function fetch(dir) {
-    shell.cd('/home/gustavo/Projects/' + dir, {'async': false})
-    await shell.exec('git fetch', {'async': true})
-    shell.echo(chalk.green('fetched /home/gustavo/Projects/' + dir))
+    shell.cd(projects_dir + dir, { async: true })
+    shell.exec('git fetch', { async: true })
+    shell.echo(chalk.green(`fetched ${projects_dir}` + dir))
 }
 
 let searches = new Promise((resolve, reject) => {
     files.forEach(async (dir) => {
-        shell.echo(chalk.blue('analisando... ') + chalk.red(dir))
+        shell.echo(chalk.blue('analising... ') + chalk.red(dir))
         await fetch(dir)
         resolve()
     })
 })
 
 searches.then(() => {
-    shell.echo(chalk.green('Feito'))
+    shell.echo(chalk.green('Done'))
 })
